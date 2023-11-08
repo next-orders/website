6d5c586b412d8eea558fd91003dd8b1a80838275
Nick Kosarev <hmbanan666@hotmail.com>
2023-11-08 13:48:20 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 
 import "./globals.scss";
 
